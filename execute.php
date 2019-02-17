cba38df080984502f13eac80f9584dbba4095a36
Falsa89 <47703851+Falsa89@users.noreply.github.com>
2019-02-17 14:17:33 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -29,6 +29,23 @@ elseif($text=="buonanotte")
 {
 	$response = "Buonanotte X";
 }
+elseif($text=="battaglia alleanza")
+{
+	$response = "Un milione!Un solo milione!Lo hai nel tuo sangue o hai dimenticato di essere un mutante? La forza di un'alleanza sta nell'impegno reciproco di ogni singolo membro a rendersi migliori ogni giorno che passa.";
+}
+elseif($text=="apertura raid")
+{
+	$response = "Miei valorosi "χ"!
+Cerebro mi ha mostrato una minaccia spaventosa.
+Un potere senza eguali si sta per abbattere su di noi e ancora una volta avremo bisogno di tutte le nostre forze per debellarlo. È un nostro dovere, il raid è appena iniziato.";
+}
+elseif($text=="raid")
+{
+	$response = "È giusto ultimare il raid?
+La risposta è dentro di te!
+Una catena è forte quando ogni singolo anello assolve al suo compito: sorreggere l'anello successivo.
+Le tue gravi mancanze mi rattristano e rendono cupo il mio animo.";
+}
 $parameters = array('chat_id' => $chatId, "text" => $response);
 $parameters["method"] = "sendMessage";
 echo json_encode($parameters);
